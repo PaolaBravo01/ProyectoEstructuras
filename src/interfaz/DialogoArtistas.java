a6fc87ba8787b7ac73cf1dc91f9b796cc506c3a0
cristianDevia <c.devia8@gmail.com>
2018-04-29 14:49:00 -0500

    Setmodal
    el frame se bloquea

--- src/interfaz/DialogoArtistas.java
+++ src/interfaz/DialogoArtistas.java
@@ -9,8 +9,6 @@ public class DialogoArtistas extends JDialog
 
 	private static final long serialVersionUID = 4567890L;
 
-	
-	
 	private InterfazAres principal;
 	
 	private PanelBanner banner;
@@ -21,14 +19,17 @@ public class DialogoArtistas extends JDialog
 		setTitle( "Artistas" );
         setSize( 520, 270 );
         setResizable( false );
-
+        setLocationRelativeTo(null);
+        setModal(true);
+        
         // Distribuidor grafico en los bordes
         setLayout( new BorderLayout( ) );        
-
+        
         banner = new PanelBanner();
         add( banner, BorderLayout.NORTH );
         
-
 	}
+	
+	
 
 }
