cdb61178cd181585af2d013310ceebda043807c7
PaolaBravo01 <36998217+PaolaBravo01@users.noreply.github.com>
2018-04-28 22:33:50 -0500

    frame que no se si es necesario

---
+++ src/interfaz/FrameApp.java
@@ -0,0 +1,17 @@
+package interfaz;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class FrameApp extends JFrame implements ActionListener
+{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
