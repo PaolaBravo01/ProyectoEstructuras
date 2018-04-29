e5128a0a46d4dbebd351d7b9a5d0452408c59074
cristianDevia <c.devia8@gmail.com>
2018-04-29 11:15:20 -0500

    algo que no sirve

--- src/interfaz/InterfazAres.java
+++ src/interfaz/InterfazAres.java
@@ -1,13 +1,52 @@
 package interfaz;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+
+import mundo.facade.Facade;
 
 public class InterfazAres extends JFrame
 {
 
+	private Facade interfazAres;
+	
+	//-----------------------------------------------------------
+	// Atributos de la interfaz
+	//-----------------------------------------------------------
+	
+	/** Panel del banner */
+	private PanelBanner panelBanner;
+	
+	public InterfazAres ( ) throws Exception
+	{
+
+		setTitle( "Ares" );
+        setSize( 757, 600 );
+        setResizable( false );
+        setDefaultCloseOperation( EXIT_ON_CLOSE );
+    
+        // Creacion de los paneles
+        panelBanner = new PanelBanner( );
+        
+        // Distribuidor grafico en los bordes
+        setLayout( new BorderLayout( ) );
+        
+        // Agrega los paneles a la interfaz
+        add( panelBanner, BorderLayout.NORTH );
+	}
+	
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+        try
+        {
+            InterfazAres interfaz = new InterfazAres( );
+            interfaz.setVisible( true );
+        }
+        catch( Exception e )
+        {
+            e.getMessage();
+        }
 
 	}
 
